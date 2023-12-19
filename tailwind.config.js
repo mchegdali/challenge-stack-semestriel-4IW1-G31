aa62602fa2a2f37bf8a6267ea579c5b466bd3375
Moustapha CHEGDALI <moustapha.chegdali@gmail.com>
2023-12-19 16:49:30 +0100

    begin quote implementation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,21 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./assets/**/*.js",
-    "./templates/**/*.html.twig",
-  ],
+  content: ['./assets/**/*.js', './templates/**/*.html.twig'],
   theme: {
     extend: {
       colors: {
         'nav-purple': '#475398',
-        'button-color': '#556CBB'
+        'button-color': '#556CBB',
+      },
+      gridTemplateColumns: {
+        subgrid: 'subgrid',
+        'quote-items': '3fr 0.5fr 0.5fr 1fr',
+      },
+      fontFamily: {
+        heading: ['"Orienta", sans-serif', ...defaultTheme.fontFamily.sans],
+        sans: ['"Roboto Flex", sans-serif', ...defaultTheme.fontFamily.sans],
       },
     },
   },
