8e34cd8e42253e6dc619df1712cc5c62cdc8c9fc
Yassine <yassine95210@hotmail.com>
2023-12-20 23:12:49 +0100

    Page facture

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,12 @@ module.exports = {
         'nav-purple': '#475398',
         'button-color': '#556CBB'
       },
-    },
+      fontSize: {
+        "small": '10px',
+      }
+    },minHeight: {
+      '250': '250px' 
+    }
   },
   plugins: [],
 }
