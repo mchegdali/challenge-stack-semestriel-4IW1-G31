447af94b7f8df215e7d24f65f3ed015432a8a714
LucasGitB <lucasaguesse@gmail.com>
2023-12-29 22:17:28 +0100

    add update for service

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./templates/**/*.html.twig",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'darkPurple': '#475398'
+      }
+    },
   },
   plugins: [],
 }
