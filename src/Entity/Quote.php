4507d0b241af1173bdf1d8f92980bdcba6fe9184
37anton <antonin.cavanne@outlook.fr>
2023-12-30 16:21:19 +0100

    Ajout entité Quote

---
+++ src/Entity/Quote.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\QuoteRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Types\UuidType;
+use Symfony\Component\Uid\Uuid;
+
+#[ORM\Entity(repositoryClass: QuoteRepository::class)]
+class Quote
+{
+    #[ORM\Id]
+    #[ORM\Column(type: UuidType::NAME, unique: true)]
+    #[ORM\GeneratedValue(strategy: 'CUSTOM')]
+    #[ORM\CustomIdGenerator(class: 'doctrine.uuid_generator')]
+    private ?Uuid $id = null;
+
+    #[ORM\OneToMany(mappedBy: 'quote', targetEntity: QuoteItem::class, orphanRemoval: true)]
+    private Collection $quoteItems;
+
+    #[ORM\ManyToOne(inversedBy: 'quotes')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Customer $customer = null;
+
+    #[ORM\ManyToOne(inversedBy: 'quotes')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?QuoteStatus $status = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\ManyToOne(inversedBy: 'quotes')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Company $company = null;
+
+
+    public function __construct()
+    {
+        $this->quoteItems = new ArrayCollection();
+    }
+
+    public function getId(): ?Uuid
+    {
+        return $this->id;
+    }
+
+    public function setId(Uuid $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, QuoteItem>
+     */
+    public function getQuoteItems(): Collection
+    {
+        return $this->quoteItems;
+    }
+
+    public function addQuoteItem(QuoteItem $quoteItem): static
+    {
+        if (!$this->quoteItems->contains($quoteItem)) {
+            $this->quoteItems->add($quoteItem);
+            $quoteItem->setQuote($this);
+        }
+
+        return $this;
+    }
+
+    public function removeQuoteItem(QuoteItem $quoteItem): static
+    {
+        if ($this->quoteItems->removeElement($quoteItem)) {
+            // set the owning side to null (unless already changed)
+            if ($quoteItem->getQuote() === $this) {
+                $quoteItem->setQuote(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function getCustomer(): ?Customer
+    {
+        return $this->customer;
+    }
+
+    public function setCustomer(?Customer $customer): static
+    {
+        $this->customer = $customer;
+
+        return $this;
+    }
+
+    public function getStatus(): ?QuoteStatus
+    {
+        return $this->status;
+    }
+
+    public function setStatus(?QuoteStatus $status): static
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getCompany(): ?Company
+    {
+        return $this->company;
+    }
+
+    public function setCompany(?Company $company): static
+    {
+        $this->company = $company;
+
+        return $this;
+    }
+
+    public function getTotalTTC()
+    {
+        $total = 0;
+        foreach ($this->quoteItems as $item) {
+            $total += $item->getQuantity() * $item->getService()->getPrice();
+        }
+        return $total;
+    }
+}
