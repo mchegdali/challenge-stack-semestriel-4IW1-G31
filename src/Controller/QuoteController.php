799090c77dd563779a6f1cf0540aebff6df0927f
37anton <antonin.cavanne@outlook.fr>
2023-12-30 16:26:37 +0100

    Ajout de la route quote_list dans le QuoteController et de sa vue : page qui affiche la liste des devis

--- src/Controller/QuoteController.php
+++ src/Controller/QuoteController.php
@@ -2,9 +2,10 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\QuoteRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class QuoteController extends AbstractController
 {
@@ -13,6 +14,27 @@ class QuoteController extends AbstractController
     {
         return $this->render('quote/index.html.twig', [
             'controller_name' => 'QuoteController',
+        ]);
+    }
+
+    #[Route('/quoteslist', name: 'quote_list')]
+    public function list(QuoteRepository $quoteRepository): Response
+    {
+        $user = $this->getUser();
+
+        if (!$user) {
+            throw $this->createNotFoundException('Utilisateur non trouvé.');
+        }
+
+        $company = $user->getCompany();
+        if (!$company) {
+            throw $this->createNotFoundException('Entreprise non trouvée.');
+        }
+
+        $quotes = $quoteRepository->findBy(['company' => $company]);
+
+        return $this->render('quote/list.html.twig', [
+            'quotes' => $quotes,
         ]);
     }
 }
