43d4e6a6d4843b18c9d45bb9c4c67d405a18968b
37anton <antonin.cavanne@outlook.fr>
2024-02-05 15:08:09 +0100

    Update champs formulaire dans création devis

--- src/Form/QuoteItemType.php
+++ src/Form/QuoteItemType.php
@@ -13,6 +13,7 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 
 class QuoteItemType extends AbstractType
 {
@@ -33,7 +34,12 @@ class QuoteItemType extends AbstractType
                     return $tax->getValue();
                 }
             ])
-            //pour priceExcludingTax faire calcul dans controller
+            ->add('priceExcludingTax', MoneyType::class, [
+                'label' => 'Prix HT',
+                'attr' => [
+                    'class' => 'w-1/4'
+                ]
+            ])
             //pour priceIncludingTax faire calcul dans controller
             //pour taxAmount faire calcul dans controller
             //on attribue quote dans le controller
