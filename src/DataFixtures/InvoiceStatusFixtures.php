5f7af34574db8b67720870ca341aa5bf3279e710
Moustapha CHEGDALI <moustapha.chegdali@gmail.com>
2024-02-07 17:31:37 +0100

    fix invoices status fixture

--- src/DataFixtures/InvoiceStatusFixtures.php
+++ src/DataFixtures/InvoiceStatusFixtures.php
@@ -10,45 +10,45 @@ class InvoiceStatusFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        $refundStatus = new InvoiceStatus();
-        $refundStatus->setName("created");
-        $refundStatus->setDisplayName("Créée");
-        $refundStatus->setBgColor("blue-600");
-        $refundStatus->setTextColor("white");
-        $refundStatus->setBorderColor("blue-600");
-        $manager->persist($refundStatus);
+        $status = new InvoiceStatus();
+        $status->setName("created");
+        $status->setDisplayName("Créée");
+        $status->setBgColor("blue-600");
+        $status->setTextColor("white");
+        $status->setBorderColor("blue-600");
+        $manager->persist($status);
 
-        $refundStatus = new InvoiceStatus();
-        $refundStatus->setName("paid");
-        $refundStatus->setDisplayName("Payée");
-        $refundStatus->setBgColor("green-700");
-        $refundStatus->setTextColor("white");
-        $refundStatus->setBorderColor("green-700");
-        $manager->persist($refundStatus);
+        $status = new InvoiceStatus();
+        $status->setName("paid");
+        $status->setDisplayName("Payée");
+        $status->setBgColor("green-700");
+        $status->setTextColor("white");
+        $status->setBorderColor("green-700");
+        $manager->persist($status);
 
-        $refundStatus = new InvoiceStatus();
-        $refundStatus->setName("late");
-        $refundStatus->setDisplayName("En retard");
-        $refundStatus->setBgColor("red-500");
-        $refundStatus->setTextColor("white");
-        $refundStatus->setBorderColor("red-500");
-        $manager->persist($refundStatus);
+        $status = new InvoiceStatus();
+        $status->setName("late");
+        $status->setDisplayName("En retard");
+        $status->setBgColor("red-500");
+        $status->setTextColor("white");
+        $status->setBorderColor("red-500");
+        $manager->persist($status);
 
-        $refundStatus = new InvoiceStatus();
-        $refundStatus->setName("cancelled");
-        $refundStatus->setDisplayName("Annulée");
-        $refundStatus->setBgColor("black");
-        $refundStatus->setTextColor("white");
-        $refundStatus->setBorderColor("black");
-        $manager->persist($refundStatus);
+        $status = new InvoiceStatus();
+        $status->setName("cancelled");
+        $status->setDisplayName("Annulée");
+        $status->setBgColor("black");
+        $status->setTextColor("white");
+        $status->setBorderColor("black");
+        $manager->persist($status);
 
-        $refundStatus = new InvoiceStatus();
-        $refundStatus->setName("refund");
-        $refundStatus->setDisplayName("Remboursée");
-        $refundStatus->setBgColor("gray-700");
-        $refundStatus->setTextColor("white");
-        $refundStatus->setBorderColor("gray-700");
-        $manager->persist($refundStatus);
+        $status = new InvoiceStatus();
+        $status->setName("refund");
+        $status->setDisplayName("Remboursée");
+        $status->setBgColor("gray-700");
+        $status->setTextColor("white");
+        $status->setBorderColor("gray-700");
+        $manager->persist($status);
 
         $manager->flush();
     }
