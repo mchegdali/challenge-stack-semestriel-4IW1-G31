945d3cfb5654032eed71ddbd57837011ff2db403
LucasGitB <lucasaguesse@gmail.com>
2024-02-09 17:32:14 +0100

    design add alpine.js

--- migrations/Version20240209151318.php
+++ migrations/Version20240209151318.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240208100600 extends AbstractMigration
+final class Version20240209151318 extends AbstractMigration
 {
     public function getDescription(): string
     {
