1c9913ef2e966c9bb83eec765016bff34517b106
Moustapha CHEGDALI <moustapha.chegdali@gmail.com>
2024-03-02 17:53:16 +0100

    fix fixture + entity + migration

--- src/DataFixtures/QuoteStatusFixtures.php
+++ src/DataFixtures/QuoteStatusFixtures.php
@@ -13,33 +13,21 @@ class QuoteStatusFixtures extends Fixture
         $status = new QuoteStatus();
         $status->setDisplayName("Brouillon");
         $status->setName("draft");
-        $status->setBgColor("grey");
-        $status->setTextColor("white");
-        $status->setBorderColor("grey");
         $manager->persist($status);
 
         $status = new QuoteStatus();
         $status->setDisplayName("Envoyé");
         $status->setName("sent");
-        $status->setBgColor("primary");
-        $status->setTextColor("white");
-        $status->setBorderColor("primary");
         $manager->persist($status);
 
         $status = new QuoteStatus();
         $status->setDisplayName("Validé");
         $status->setName("accepted");
-        $status->setBgColor("success");
-        $status->setTextColor("white");
-        $status->setBorderColor("success");
         $manager->persist($status);
 
         $status = new QuoteStatus();
         $status->setDisplayName("Refusé");
         $status->setName("refused");
-        $status->setBgColor("error");
-        $status->setTextColor("white");
-        $status->setBorderColor("error");
         $manager->persist($status);
 
         $manager->flush();
