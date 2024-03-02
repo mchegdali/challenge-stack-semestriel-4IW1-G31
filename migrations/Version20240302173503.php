4df9cfbc48bfccd2ac8e97b8c6b1c0dd891952c1
Moustapha CHEGDALI <moustapha.chegdali@gmail.com>
2024-03-02 18:35:06 +0100

    test file watcher

--- migrations/Version20240302173503.php
+++ migrations/Version20240302173503.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240302172733 extends AbstractMigration
+final class Version20240302173503 extends AbstractMigration
 {
     public function getDescription(): string
     {
